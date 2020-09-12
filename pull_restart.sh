49523e3407e3195984927cc824a5585c55f67a46
tknzk <info@tknzk.dev>
2020-09-12 13:47:01 +0900

    remove sleeep

--- pull_restart.sh
+++ pull_restart.sh
@@ -3,5 +3,5 @@ cd /home/isucon/isuumo
 git pull
 cd /home/isucon/isuumo/webapp/ruby/
 bundle install && sudo systemctl stop isuumo.ruby && sudo systemctl start isuumo.ruby
-sleep(2)
+# sleep(2)
 sudo systemctl status isuumo.ruby
