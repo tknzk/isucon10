5e73ae38f0ebe5a1542ffabad13bfada9433d497
tknzk <info@tknzk.dev>
2020-09-12 19:43:52 +0900

    commentout

--- ruby/unicorn.conf.rb
+++ ruby/unicorn.conf.rb
@@ -4,4 +4,4 @@ listen File.expand_path('../tmp/unicorn.sock', __FILE__)
 worker_processes (ENV.fetch('WORKER_NUM', 16)).to_i
 
 stderr_path File.expand_path('../log/unicorn_stderr.log', __FILE__)
-stdout_path File.expand_path('../log/unicorn_stdout.log', __FILE__)
+# stdout_path File.expand_path('../log/unicorn_stdout.log', __FILE__)
