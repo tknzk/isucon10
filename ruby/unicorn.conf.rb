5c43657c2e4432b9340b49b69bafc31b7477b54d
tknzk <info@tknzk.dev>
2020-09-12 19:20:00 +0900

    process

--- ruby/unicorn.conf.rb
+++ ruby/unicorn.conf.rb
@@ -1,7 +1,7 @@
 # listen (ENV.fetch('SERVER_PORT', 1323)).to_i
 listen File.expand_path('../tmp/unicorn.sock', __FILE__)
 
-worker_processes (ENV.fetch('WORKER_NUM', 16)).to_i
+worker_processes (ENV.fetch('WORKER_NUM', 32)).to_i
 
 stderr_path File.expand_path('../log/unicorn_stderr.log', __FILE__)
 stdout_path File.expand_path('../log/unicorn_stdout.log', __FILE__)
