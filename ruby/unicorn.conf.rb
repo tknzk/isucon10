ba8ebbacff6e4b4df0ee4abd82f0ccde02b515bb
tknzk <info@tknzk.dev>
2020-09-12 17:10:02 +0900

    log

--- ruby/unicorn.conf.rb
+++ ruby/unicorn.conf.rb
@@ -1,3 +1,6 @@
 listen (ENV.fetch('SERVER_PORT', 1323)).to_i
 
 worker_processes (ENV.fetch('WORKER_NUM', 16)).to_i
+
+stderr_path File.expand_path('../log/unicorn_stderr.log', __FILE__)
+stdout_path File.expand_path('../log/unicorn_stdout.log', __FILE__)
